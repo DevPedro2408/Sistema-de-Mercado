261911a25bb8361e3c844111665cfa7e6007ff28
Pedro Augusto <pe.augusto2408@gmail.com>
2026-04-29 18:53:56 -0300

    Inicio
    Só para salvar mesmo

---
+++ Js/produtos.js
@@ -0,0 +1,14 @@
+//Leitor de código de barras
+const codeReader = new ZXing.BrowserBarcodeReader()
+
+codeReader.decodeFromVideoDevice(null, 'camera', (result, err) => {
+
+    if (result) {
+
+        let codigo = result.text
+
+        document.getElementById("codigo").value = codigo
+
+        console.log("Código detectado:", codigo)
+    }
+})
