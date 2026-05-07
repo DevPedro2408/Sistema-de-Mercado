099f1f394e0297e898e60c6a5bbfd6e9f5d65f26
Pedro Augusto <pe.augusto2408@gmail.com>
2026-05-07 17:56:41 -0300

    atualizaçao07/5

--- Js/produtos.js
+++ Js/produtos.js
@@ -1,20 +1,46 @@
+let codigoDeBarras = document.getElementById("codigo-barras")
+let codigoInterno = document.getElementById("codigo-interno")
+let descricao = document.getElementById("descricao")
+let categoria = document.getElementById("categoria")
+let quantidadeEstoque = document.getElementById("quantidade-estoque")
+let estoqueMinimo = document.getElementById("estoque-minimo")
+let precoVenda = document.getElementById("preco-venda")
 
+produtos = []
+
+function salvar() {
+    
+}
+
+function novoProduto() {
+    let codigoBarras = Number(codigoDeBarras.value)
+
+    console.log(codigoDeBarras)
+}
+
+function cancelar() {
+
+}
+
+function excluir() {
+
+}
 
 
 
 
 
 //Leitor de código de barras
-const codeReader = new ZXing.BrowserBarcodeReader()
+// const codeReader = new ZXing.BrowserBarcodeReader()
 
-codeReader.decodeFromVideoDevice(null, 'camera', (result, err) => {
+// codeReader.decodeFromVideoDevice(null, 'camera', (result, err) => {
 
-    if (result) {
+//     if (result) {
 
-        let codigo = result.text
+//         let codigo = result.text
 
-        document.getElementById("codigo").value = codigo
+//         document.getElementById("codigo").value = codigo
 
-        console.log("Código detectado:", codigo)
-    }
-})
+//         console.log("Código detectado:", codigo)
+//     }
+// })
