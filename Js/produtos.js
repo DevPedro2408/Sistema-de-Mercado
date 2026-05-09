417693acb2b56bbed9fa4fcb07ae178d8813abf3
Pedro Augusto <pe.augusto2408@gmail.com>
2026-05-09 16:17:07 -0300

    Atualização 09/05

--- Js/produtos.js
+++ Js/produtos.js
@@ -5,28 +5,47 @@ let categoria = document.getElementById("categoria")
 let quantidadeEstoque = document.getElementById("quantidade-estoque")
 let estoqueMinimo = document.getElementById("estoque-minimo")
 let precoVenda = document.getElementById("preco-venda")
+let salvar = document.getElementById("salvar")
+let ProdutoNovo = document.getElementById("novo-produto")
+let cancelar = document.getElementById("cancelar")
+let excluir = document.getElementById("excluir")
+let inputsCodigoBarras = document.querySelectorAll(".inputsCodigoBarras")
 
-produtos = []
-
-function salvar() {
-    
-}
+let produtos = []
 
 function novoProduto() {
     let codigoBarras = Number(codigoDeBarras.value)
+    let cdInterno = Number(codigoInterno.value)
+    let precoVendaValue = Number(precoVenda.value)
+    let estoqueValue = Number(quantidadeEstoque.value)
+    let estoqueMinimoValue = Number(estoqueMinimo.value)
 
-    console.log(codigoDeBarras)
+    let statusResul = "Acabando"
+    let acoesResul = "Excluir"
+
+    let campoVazio = false
+
+    inputsCodigoBarras.forEach(inputElementos => {
+        if(inputElementos.value === "") {
+            return campoVazio = true
+        }
+    })
+
+    if (!campoVazio) {
+        produtos.push({
+                codigoBarrasObj: codigoBarras, 
+                codigoInternoObj: cdInterno, 
+                descricaoObj: descricao.value, 
+                precoVendaObj: precoVendaValue, 
+                estoqueObj: estoqueValue,
+                estoqueMinimoObj: estoqueMinimoValue, 
+                statusObj: statusResul,
+                acoes: acoesResul,
+                categoriaObj: categoria.value
+        })
+    }
+    console.log(produtos)
 }
-
-function cancelar() {
-
-}
-
-function excluir() {
-
-}
-
-
 
 
 
